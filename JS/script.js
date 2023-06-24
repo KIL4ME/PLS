88ef76778aedf51c2c328f203a8a4a1f0ea1889b
KIL4ME <pdo323@yahoo.com>
2023-06-24 12:49:38 -0700

    researching notes

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,29 @@
 function processForm(event) {
     event.preventDefault();
 }
+const form = document.getElementById('survey-form');
+const answers = {
+    question1: form.elements.question1.value,
+    question2: form.elements.question2.value,
+
+};
+
+const suggestion =suggestLanguage(answers);
+
+showResult(suggestion);
+
+function suggestLanguage(answers) {
+  
+if (answers.question1 === 'option1') {
+    return languages[0];
+} else if (answers.question2 === 'option2') {
+    return languages[1];
+} else {
+    return languages[2];
+}
+}
+
+function showResult(suggestion) {
+    const resultContainer = document.getElementById(result);
+    resultContainer.style.display = 'block';
+}
