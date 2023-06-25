2ded4ec634b1b72d961bb61c2568bf4fe84b7b1a
KIL4ME <pdo323@yahoo.com>
2023-06-25 08:20:18 -0700

    revising script.js

--- JS/script.js
+++ JS/script.js
@@ -10,20 +10,45 @@ const answers = {
 
 const suggestion =suggestLanguage(answers);
 
+saveAnswers(answers);
+
 showResult(suggestion);
+}
+
 
 function suggestLanguage(answers) {
-  
-if (answers.question1 === 'option1') {
-    return languages[0];
-} else if (answers.question2 === 'option2') {
-    return languages[1];
-} else {
-    return languages[2];
-}
+const storedAnswers = getStoredAnswers();
+
+// Use the stored answers for branching logic if available
+if (storedAnswers) {
+  // Example branching logic using stored answers
+  if (storedAnswers.question1 === 'option1') {
+    // Suggestion based on stored answer to question 1
+    return languages[0]; // Replace with appropriate language object
+  } else if (storedAnswers.question2 === 'option2') {
+    // Suggestion based on stored answer to question 2
+    return languages[1]; // Replace with appropriate language object
+  }
 }
 
-function showResult(suggestion) {
-    const resultContainer = document.getElementById(result);
-    resultContainer.style.display = 'block';
+// If no stored answers or no specific suggestion, use default suggestion
+return languages[2]; // Replace with appropriate language object
 }
+
+// Function to store the user's answers in local storage
+function saveAnswers(answers) {
+localStorage.setItem('surveyAnswers', JSON.stringify(answers));
+}
+
+// Function to retrieve the stored answers from local storage
+function getStoredAnswers() {
+const storedAnswers = localStorage.getItem('surveyAnswers');
+return storedAnswers ? JSON.parse(storedAnswers) : null;
+}
+
+// ...
+
+// Add an event listener to clear the stored answers when the page loads
+window.addEventListener('load', function() {
+localStorage.removeItem('surveyAnswers');
+});
